4a41896b86fc4e75d70283f05e19c59602f5597d
Benjamin Herson <benjaminherson@gmail.com>
2015-02-09 12:30:48 -0800

    cleanup scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var queenAttack = function(queen_coords, opponent_coords) {
-  if ((queen_coords[1] === opponent_coords[1]) || (queen_coords[0] === opponent_coords[0])) {
+  if (queen_coords[1] === opponent_coords[1] || queen_coords[0] === opponent_coords[0]) {
     return true;
-  } else if ((Math.abs(queen_coords[0] - opponent_coords[0])) === (Math.abs(queen_coords[1] - opponent_coords[1]))) {
+  } else if (Math.abs(queen_coords[0] - opponent_coords[0]) === Math.abs(queen_coords[1] - opponent_coords[1])) {
     return true;
   } else {
   return false;
