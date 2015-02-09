0f9156051f24bbd96ac89249d4bdf02c7224c91b
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-09 15:12:22 -0800

    add web functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,26 @@ var queenAttack = function(queen_coords, opponent_coords) {
   return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#queen-attack").submit(function(event) {
+
+    var queenCoordinates = $("input#queen-coordinates").val().split(",");
+    var opponentCoordinates = $("input#opponent-coordinates").val().split(",");
+    var result = queenAttack(queenCoordinates, opponentCoordinates);
+
+    $(".queen-coordinates").text(queenCoordinates);
+    $(".opponent-coordinates").text(opponentCoordinates);
+
+    if (result) {
+      $(".answer").text("can");
+    } else {
+      $(".answer").text("can't");
+    }
+
+    $("#result").show();
+
+    event.preventDefault();
+
+  });
+});
